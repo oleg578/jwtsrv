8b9a3af1cbe71a6d323b4f3ea722eac4dbc99b43
oleg578 <oleg.nagornij@gmail.com>
2019-08-06 18:08:03 +0300

    general renew token

--- router/renew.go
+++ router/renew.go
@@ -2,9 +2,12 @@ package router
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
+
+	"../config"
+	"github.com/oleg578/jwts"
+	jwt "github.com/oleg578/jwts"
 )
 
 // Renew route
@@ -28,6 +31,43 @@ func RenewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	//get and test email
 	refreshToken := r.Form.Get("refresh_token")
-	log.Println(refreshToken)
+	rtok, err := jwt.Parse(refreshToken)
+	if err != nil {
+		err := fmt.Errorf("token was not parsed")
+		ResponseBuild(w, APIResp{Response: "", Error: err.Error()})
+		return
+	}
+	errV := rtok.Validate(config.SecretKey)
+	if errV != nil {
+		err := fmt.Errorf("token is not valid")
+		ResponseBuild(w, APIResp{Response: "", Error: err.Error()})
+		return
+	}
+	//token valid - we can build new
+	payload := rtok.Payload
+	tm := time.Now()
+	texp := tm.Add(time.Minute * config.AccessDuration)
+	tref := tm.Add(time.Minute * config.RefreshDuration)
+
+	payload["exp"] = texp.Unix()
+	AccessToken, err := jwts.CreateTokenHS256(payload, config.SecretKey)
+	if err != nil {
+		time.Sleep(time.Second * 5)
+		ResponseBuild(w, APIResp{Response: "", Error: err.Error()})
+		return
+	}
+	payload["exp"] = tref.Unix()
+	RefreshToken, errRef := jwts.CreateTokenHS256(payload, config.SecretKey)
+	if errRef != nil {
+		ResponseBuild(w, APIResp{Response: "", Error: errRef.Error()})
+		return
+	}
+	Resp.Response = struct {
+		AccessToken  string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+	}{
+		AccessToken.RawStr,
+		RefreshToken.RawStr,
+	}
 	ResponseBuild(w, Resp)
 }
