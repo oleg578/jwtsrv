fe80145f6ed7ee341686205a0e04287f259af1a7
oleg578 <oleg.nagornij@gmail.com>
2020-10-11 11:14:27 +0300

    change IP

--- config/config.go
+++ config/config.go
@@ -6,8 +6,8 @@ const (
 	//Host = "10.132.146.197"
 	Host = ""
 
-	RedisDSN = `192.168.1.20:6379`
-	RedisDB  = 2
+	RedisDSN = `192.168.1.121:6379`
+	RedisDB  = 0
 
 	//SecretKey       = "3dp9gudw0l19yr9ois8iu9b3220qemn8"
 	AccessDuration  = 1440 * 1000  // 24 hour
