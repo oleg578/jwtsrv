1269ade5e1a8b197d99bcce59a996a1f9fe75cc9
oleg578 <oleg.nagornij@gmail.com>
2020-12-22 20:27:40 +0200

    autocert let's encrypt

--- config/config.go
+++ config/config.go
@@ -1,13 +1,17 @@
 package config
 
 const (
+	AdminMail = "oleg.nagornij@gmail.com"
+	Domain    = ""
+	CertPath  = "/etc/autocert/ssl/"
+
 	MAXBODYLENGTH = 2048
 
 	//Host = "10.132.146.197"
-	Host = ""
+	//Host = ""
 
 	RedisDSN = `192.168.1.20:6379`
-	RedisDB  = 0
+	//RedisDB  = 0
 
 	//SecretKey       = "3dp9gudw0l19yr9ois8iu9b3220qemn8"
 	AccessDuration  = 1440 * 1000  // 24 hour
--- jwtsrv.go
+++ jwtsrv.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/oleg578/jwtsrv/config"
 	"github.com/oleg578/jwtsrv/router"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -27,15 +29,28 @@ func main() {
 	mux.Handle("/renew", renewHandler)
 
 	//server
-	//for cluster
+	//certManager
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(config.Domain),
+		Cache:      autocert.DirCache(config.CertPath),
+		Email:      config.AdminMail,
+	}
+
+	//server
 	srv := &http.Server{
-		Addr:           config.Host + ":5000",
+		Addr: ":https", // production
+		//Addr:           ":8000", // dev
 		Handler:        mux,
 		ReadTimeout:    60 * time.Second,
 		WriteTimeout:   60 * time.Second,
 		MaxHeaderBytes: 1 << 20,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
 	}
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalln(err)
-	}
+	//https only
+	log.Fatal(srv.ListenAndServeTLS("", ""))
+	//local debug
+	//log.Fatal(srv.ListenAndServe())
 }
