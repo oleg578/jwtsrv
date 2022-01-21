f7ee85e830a1d99d836e081b973bba46f280810d
oleg578 <oleg.nagornij@gmail.com>
2022-01-21 16:30:16 +0200

    fix

--- phplib/jwt.php
+++ phplib/jwt.php
@@ -28,14 +28,27 @@ if (isset($_GET["code"])) {
 if (isset($_GET["code"])) {
     echo "<pre>";
     var_dump($tokens->access_token);
+    echo "</pre>";
+    echo "<hr>";
 
     $jwtToken = $tokens->access_token;
 
     $jwtArr = array_combine(['header', 'payload', 'signature'], explode('.', $jwtToken));
 
+    echo "<pre>";
     var_dump($jwtArr);
-    echo "<hr>";
-    print_r(base64_decode($jwtArr['header']));
-    print_r(base64_decode($jwtArr['payload'])); // token payload
     echo "</pre>";
+    echo "<hr>";
+    echo "<pre>";
+    print_r(base64_decode($jwtArr['header']));
+    echo "</pre>";
+    echo "<hr>";
+    echo "<pre>";
+    $payload = base64_decode($jwtArr['payload']);
+    $user = json_decode($payload);
+    print_r($user); // token payload
+    echo "</pre>";
+    echo "<hr>";
+    echo "<p>User: ". $user->eml . "</p>";
+    echo "<p>Expiration: ". gmdate(DateTime::ISO8601, $user->exp) . "</p>";
 }
